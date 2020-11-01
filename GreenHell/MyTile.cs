e3c9dae74b16a6893bb6733b972b46d0d69fe967
hamstar0 <webutek@gmail.com>
2020-11-01 11:02:33 -0800

    v0.4.0.1 - Fixed incorrect KillTile conditions

--- GreenHell/MyTile.cs
+++ GreenHell/MyTile.cs
@@ -8,7 +8,7 @@ using GreenHell.Logic;
 namespace GreenHell {
 	class GreenHellTile : GlobalTile {
 		public override void KillTile( int i, int j, int tileType, ref bool fail, ref bool effectOnly, ref bool noItem ) {
-			if( fail || effectOnly || /*noItem ||*/ Main.netMode == NetmodeID.MultiplayerClient ) {
+			if( effectOnly || noItem || Main.netMode == NetmodeID.MultiplayerClient ) {
 				return;
 			}
 			if( WorldGen.noTileActions || WorldGen.gen ) {
